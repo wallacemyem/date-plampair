54d076898298a3fe27496ff0b420b7fbcf69c104
Wallace Myem <wallacemyem@hotmail.com>
2022-02-15 15:15:27 +0100

    firebase

--- js/firebase/config.js
+++ js/firebase/config.js
@@ -3,11 +3,11 @@
 // Must replace to you own config
     
 var firebaseConfig = {
-    apiKey: "AIzaSyA47GVUCmQ34DsjXIlY1qXAzKweX0ck6r773o",
-    authDomain: "dating-app-234234.firebaseapp.com",
-    databaseURL: "https://dating-app-234234.firebaseio.com",
-    projectId: "dating-app-234234",
-    storageBucket: "dating-app-23423424.appspot.com",
-    messagingSenderId: "345345345345",
-    appId: "1:345345345:web:f25d825c154d59f41f485def"
+    apiKey: "AIzaSyCyA4r0--klWB12UGwczKEGqthALDjZl6Q",
+    authDomain: "palmpair.firebaseapp.com",
+    projectId: "palmpair",
+    storageBucket: "palmpair.appspot.com",
+    messagingSenderId: "70570251400",
+    appId: "1:70570251400:web:6c0c2735a136c2bfe7bb3d",
+    measurementId: "G-JN0YTSJS5D"
 };
